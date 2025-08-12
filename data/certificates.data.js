cbc974ba2c7e4c02b2efb90ab3c059c1ba07f901
18-Madhav-9 <18-Madhav-9@users.noreply.github.com>
2025-08-12 09:10:00 +0530

    mock data for article/blog page

---
+++ data/certificates.data.js
@@ -0,0 +1,20 @@
+[
+  {
+    "title": "CS50: Introduction to Computer Science",
+    "issuer": "Harvard University",
+    "date": "Dec 2023",
+    "link": "https://pll.harvard.edu/course/cs50-introduction-computer-science"
+  },
+  {
+    "title": "Full Stack Open",
+    "issuer": "University of Helsinki",
+    "date": "Jan 2024",
+    "link": "https://fullstackopen.com/en/"
+  },
+  {
+    "title": "React Native Specialist",
+    "issuer": "Meta",
+    "date": "Mar 2024",
+    "link": "https://www.coursera.org/professional-certificates/meta-react-native"
+  }
+]
