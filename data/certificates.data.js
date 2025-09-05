6f7aa3054af765f1fe3656d9a67a236ef1e867fc
18-Madhav-9 <18-Madhav-9@users.noreply.github.com>
2025-09-05 12:42:19 +0530

    add my cs50 certificate

--- data/certificates.data.js
+++ data/certificates.data.js
@@ -3,18 +3,6 @@
     "title": "CS50: Introduction to Computer Science",
     "issuer": "Harvard University",
     "date": "Dec 2023",
-    "link": "https://pll.harvard.edu/course/cs50-introduction-computer-science"
+    "link": "https://cs50.harvard.edu/certificates/543adab8-7b95-4038-8ef4-8300a3686f93"
   },
-  {
-    "title": "Full Stack Open",
-    "issuer": "University of Helsinki",
-    "date": "Jan 2024",
-    "link": "https://fullstackopen.com/en/"
-  },
-  {
-    "title": "React Native Specialist",
-    "issuer": "Meta",
-    "date": "Mar 2024",
-    "link": "https://www.coursera.org/professional-certificates/meta-react-native"
-  }
 ]
