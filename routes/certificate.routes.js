390a38ea537e7e4558b99b99a9094e767018810e
18-Madhav-9 <mash2172003@gmail.com>
2025-09-06 12:47:08 +0530

    backend files moved

--- routes/certificate.routes.js
+++
@@ -1,7 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const certificateController = require('../controllers/certificate.controller');
-
-router.get('/', certificateController.fetchAllCertificates);
-
-module.exports = router;
--- routes/contact.routes.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const { handleContactSubmit } = require('../controllers/contact.controller');
-
-const router = express.Router();
-
-router.post('/', handleContactSubmit);
-
-module.exports = router;
