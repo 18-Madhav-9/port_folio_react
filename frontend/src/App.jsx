cb1ae93d8f673d5d9ab524e025e41deef5ea8e50
18-Madhav-9 <mash2172003@gmail.com>
2025-09-21 15:03:28 +0530

    configure main application layout and routing

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,9 +1,34 @@
 import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import ThemeProvider from "./context/ThemeContext";
+import AppContent from "./components/layout/AppContent";
 
-const App = () =>{
+import Home from "./pages/Home";
+import Projects from "./pages/Projects";
+import Contact from "./pages/Contact";
+import Stack from "./pages/Stack";
+import Stats from "./pages/Stats";
+import Thoughts from "./pages/Thoughts";
+import ArticleDetail from "./components/common/ArticleDetail"
+
+const App = () => {
   return (
-    <div>
-      PortFolio
-    </div>
-  )
-}
+    <ThemeProvider>
+      <Router>
+        <AppContent>
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/projects" element={<Projects />} />
+            <Route path="/contact" element={<Contact />} />
+            <Route path="/stats" element={<Stats />} />
+            <Route path="/thoughts" element={<Thoughts />} />
+            <Route path="/thoughts/:slug" element={<ArticleDetail />} />
+            <Route path="/stack" element={<Stack />} />
+          </Routes>
+        </AppContent>
+      </Router>
+    </ThemeProvider>
+  );
+};
+
+export default App;
